b28c9e2af576a6486de2be2fb1f01f25f2fa9e10
Dairo Estiben Beltrán <daiydan99@hotmail.com>
2018-02-27 09:32:44 -0500

    arreglado

--- ejercicioDispensador/src/principal/Productos.java
+++ ejercicioDispensador/src/principal/Productos.java
@@ -9,6 +9,33 @@ package principal;
  *
  * @author daiyd
  */
+
 public class Productos {
-    
+    class Margarita{
+    }
+    class Galletas{
+    }
+class Chitos{
+}
+class Chocorramo{
+    int horas;
+    int dias;
+    int semanas;
+    int min;
+    //setSemanas permitirá guardar el dato de las semanas totales según los cálculos en la función principal
+    void setSemanas (int a){
+        semanas = a;
+    }
+    //setDias permitirá, al igual que setSemanas, guardar el dato correspondiente según los cálculos correspondientes
+    void setDias(int a){
+        dias = a;
+    }
+    //setHoras guardará las horas residuales después de haber realizado los demás cálculos
+    void setHoras(int a){
+        horas = a;
+    }
+        void setMin(int a){
+        min = a;
+    }
+}    
 }
